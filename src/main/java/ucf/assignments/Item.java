ddbbc4c25ce0638343757c068fed6137e94c09ad
nick-vigg <nviggiani@knights.ucf.edu>
2021-07-22 13:31:42 -0400

    Item commit

--- src/main/java/ucf/assignments/Item.java
+++ src/main/java/ucf/assignments/Item.java
@@ -17,7 +17,19 @@ public class Item {
     //setter method for ID
     public void setId(String id) {
         //use this.id = id to set id value for instance of this item
-        this.id = id;
+        //Set ERROR messages is length of string is not 10 OR
+        // if the String does not contain exclusively characters and numbers
+        if (id.length() == 10){
+            this.id = id;
+        } else {
+            this.id = "ERROR, Serial Number must be formatted XXXXXXXXXX";
+        }
+        char idList[] = id.toCharArray();
+        for (int i = 0; i < idList.length; i++){
+            if (!Character.isLetterOrDigit(idList[i])){
+                this.id = "ERROR, Serial Number must only contain Num/Char";
+            }
+        }
     }
 
     //getter method for value
@@ -26,9 +38,10 @@ public class Item {
     }
 
     //setter method for value
-    public void setValue(BigDecimal value) {
+    public void setValue(String value) {
         //use this.value = value to set value for instance of this item
-        this.value = value;
+        //convert from String to BigDecimal type
+        this.value = new BigDecimal(value);
     }
 
     //getter method for name
@@ -39,7 +52,12 @@ public class Item {
     //setter method for name
     public void setName(String name) {
         //use this.name = name to set name value for instance of this item
-        this.name = name;
+        //Set ERROR message if name is not between 2 and 256 characters
+        if(name.length() >= 2 && name.length() <= 256){
+            this.name = name;
+        } else {
+            this.name = "ERROR name must be between 2 and 256 characters inclusive";
+        }
     }
 
 }
